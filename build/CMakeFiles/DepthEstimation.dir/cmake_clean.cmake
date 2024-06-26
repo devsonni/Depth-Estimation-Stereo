193843266ae3525a472e8bcd98d68fded5b8647f
Dev Soni <devsoni44.pp@gmail.com>
2024-06-26 03:13:51 -0400

    first commit

---
+++ build/CMakeFiles/DepthEstimation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DepthEstimation.dir/src/Main.cpp.o"
+  "CMakeFiles/DepthEstimation.dir/src/Main.cpp.o.d"
+  "DepthEstimation"
+  "DepthEstimation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DepthEstimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Main.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    cv::Mat image;
+    image = cv::imread("../src/test.jpg", cv::IMREAD_COLOR);
+
+    if (image.empty()) {
+        std::cerr << "Could not open or find the image" << std::endl;
+        return -1;
+    }
+
+    cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display window", image);
+
+    cv::waitKey(0);
+    return 0;
+}
